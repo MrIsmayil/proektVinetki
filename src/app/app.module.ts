f0af92831b61b99dd1174450dfa43a09429cd6be
MrIsmayil <mrismayil2012@gmail.com>
2020-03-12 15:32:02 +0300

    Modify-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadingComponent } from './components/heading/heading.component';
+import { PreschoolComponent } from './components/preschool/preschool.component';
+import { ElementarySchoolComponent } from './components/elementary-school/elementary-school.component';
+import { SchoolComponent } from './components/school/school.component';
+
+const appRoutes: Routes = [
+  {path:'', component:HeadingComponent},
+  {path:'preschool', component:PreschoolComponent},
+  {path:'elementary', component:ElementarySchoolComponent},
+  {path:'schools', component:SchoolComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingComponent,
+    PreschoolComponent,
+    ElementarySchoolComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
